f54c7f3e4991267797814df25819aa2c7cb49b03
Laura Pereda <lypereda17@gmail.com>
2020-01-30 20:45:06 -0600

    Generate getters and setters of StudentUser

--- app/src/main/java/com/example/studybuddy_prototype_one/StudentUser.java
+++ app/src/main/java/com/example/studybuddy_prototype_one/StudentUser.java
@@ -10,4 +10,44 @@ public class StudentUser {
     private String email;
     private String password;
     private ArrayList<Course> courses;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public ArrayList<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(ArrayList<Course> courses) {
+        this.courses = courses;
+    }
 }
