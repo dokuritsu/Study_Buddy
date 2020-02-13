85f14ece108715ab35239d3f00ae882006eadd3f
Laura Pereda <lypereda17@gmail.com>
2020-02-13 12:04:07 -0600

    Created new activity called Dashboard

---
+++ app/src/main/java/com/example/studybuddy_prototype_one/Dashboard.java
@@ -0,0 +1,14 @@
+package com.example.studybuddy_prototype_one;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
